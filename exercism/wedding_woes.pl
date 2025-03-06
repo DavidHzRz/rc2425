34cdeb8d4bc120c9e6bee833a4ea9d95560338c6
DavidHzRz <david.hernandez1@alu.uhu.es>
2025-03-06 09:14:22 +0100

    Update wedding_woes.pl

--- exercism/wedding_woes.pl
+++ exercism/wedding_woes.pl
@@ -10,4 +10,4 @@ pairing(A, _):- chatty(A).
 pairing(_, A):- chatty(A).
 pairing(A, B):- likes(A,B), likes(B,A).
 
-seating(A, B, B, D, E):- pairing(A,B), pairing(B,C), pairing(C,D), pairing(D,E), pairing(E,A).
+seating(A, B, C, D, E):- pairing(A,B), pairing(B,C), pairing(C,D), pairing(D,E), pairing(E,A).
