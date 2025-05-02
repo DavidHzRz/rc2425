8f8a89b8515bbb89497a0c034cf438d9c4ecbbd9
DavidHzRz <david.hernandez1@alu.uhu.es>
2025-05-02 10:40:59 +0200

    Create hamming.pl

---
+++ exercism/hamming.pl
@@ -0,0 +1,13 @@
+hamming_distance(Str1, Str2, Dist):- 
+    string_codes(Str1, L1), 
+    string_codes(Str2, L2),
+    diferencias(L1, L2, Dist).
+
+diferencias([], [], 0).
+diferencias([Cab1|Resto1], [Cab2|Resto2], Dist):- 
+    diferentes(Cab1, Cab2, R1),
+    diferencias(Resto1, Resto2, R2),
+    Dist is R1 + R2.
+
+diferentes(A, B, 1):- A \= B.
+diferentes(_, _, 0).
