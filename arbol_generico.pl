09a1dc5e454faec05bcd2cbb0e5d880d47482aff
DavidHzRz <david.hernandez1@alu.uhu.es>
2025-05-09 11:26:45 +0200

    Create arbol_generico.pl

---
+++ arbol_generico.pl
@@ -0,0 +1,20 @@
+/*
+Árbol genérico
+
+Caso base:
+	n0 = a(_ [])
+*/
+
+% Conexión Arbol con ListaHijos
+
+cuenta_nodos(a(_, ListaHijos), R):-
+	cuenta_nodos(ListaHijos, RLH),
+	R is RLH + 1.
+	
+% Lista de Arboles
+
+cuenta_nodos([], 0).
+cuenta_nodos([Cab|Resto], ):-
+	cuenta_nodos(Resto, RR),
+	cuenta_nodos(Cab, RCa),
+	R is RR + RCa.
