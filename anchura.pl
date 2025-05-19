b0ee710acaa32251ce6286e606b1ab3c740a58d6
DavidHzRz <david.hernandez1@alu.uhu.es>
2025-05-19 15:06:17 +0200

    Update anchura.pl

--- anchura.pl
+++ anchura.pl
@@ -23,23 +23,19 @@ anchura([a(E, ListaHijos)|Resto], [E|R2]):-
 	anchura(R, R2).
 
 /*
-
+(EXAMEN)
 bin2gen(ArbolBinario, ArbolGenerico)
-  es cierto si ArbolGenerico unifica con un arbol
-  genérico equivalente a ArbolBinario
-
+	es cierto si ArbolGenerico unifica con un árbol
+	genérico equivalente a ArbolBinario
 */
 
 bin2gen(a(Et, nil, nil), a(Et, [])).
-
 bin2gen(a(Et, nil, HD), a(Et, [RD])):- HD \= nil,
-  bin2gen(HD, RD).
-
+	bin2gen(HD, RD).
 bin2gen(a(Et, HI, nil), a(Et, [RI])):- HI \= nil,
-  bin2gen(HI, RI).
-
+	bin2gen(HI, RI).
 bin2gen(a(Et, HI, HD), a(Et, [RI, RD])):- 
- HI \= nil, 
- HD \= nil,
- bin2gen(HI, RI),
- bin2gen(HD, RD).  
+	HI \= nil, 
+	HD \= nil,
+	bin2gen(HI, RI),
+	bin2gen(HD, RD).
