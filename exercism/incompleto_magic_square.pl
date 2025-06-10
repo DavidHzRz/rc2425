a4b0b64b170842e1dbf0c3d97c8b7ad40edd24e1
DavidHzRz <david.hernandez1@alu.uhu.es>
2025-06-10 12:54:37 +0200

    Update incompleto_magic_square.pl

--- exercism/incompleto_magic_square.pl
+++ exercism/incompleto_magic_square.pl
@@ -1,23 +1,55 @@
-matrix1([
-            [2, 7, 6],
-  			[9, 5, 1],
-  			[4, 3, 8]
-        ]).
-
 :- use_module(library(clpfd)).
 
-magic_square(M):-
-    suma_filas(M, R),
-    suma_columnas(M, R).
-    %suma_diagonales(M, RD),
-    %RC = RD.
+magic_square(Square) :-
+    % Verifica que Square sea una matriz cuadrada (N x N)
+    length(Square, N),
+    maplist(length_list(N), Square), % Todas las filas tienen longitud N
 
-suma_filas([], []).
-suma_filas([Cab|Resto], [R1|R2]):-
-    sum_list(Cab, R1),
-    suma_filas(Resto, R2).
+    % Calcula suma de la primera fila
+    nth0(0, Square, FirstRow),
+    sum_list(FirstRow, MagicSum),
 
-suma_columnas(M, R):-
-    transpose(M, M2),
-    suma_filas(M2, R).
-        
+    % Verifica sumas de filas
+    maplist(sum_row_equals(MagicSum), Square),
+
+    % Verifica sumas de columnas
+    transpose(Square, Transposed),
+    maplist(sum_row_equals(MagicSum), Transposed),
+
+    % Verifica diagonales
+    diagonal_principal(Square, Diag1),
+    sum_list(Diag1, MagicSum),
+    diagonal_secundaria(Square, Diag2),
+    sum_list(Diag2, MagicSum).
+
+
+% Verifica que una lista tenga longitud N
+length_list(N, List) :- length(List, N).
+
+% Verifica que la suma de una fila sea igual a MagicSum
+sum_row_equals(MagicSum, Row) :- sum_list(Row, MagicSum).
+
+% Obtiene la diagonal principal (de arriba-izquierda a abajo-derecha)
+diagonal_principal([], []).
+diagonal_principal([Row|Rows], [Element|Diagonal]) :-
+    nth0(0, Row, Element),
+    maplist(remove_first, Rows, RowsWithoutFirst),
+    diagonal_principal(RowsWithoutFirst, Diagonal).
+
+remove_first([_|Tail], Tail).
+
+% Obtiene la diagonal secundaria (de arriba-derecha a abajo-izquierda)
+diagonal_secundaria(Square, Diagonal) :-
+    length(Square, N),
+    diagonal_secundaria_aux(Square, 0, N, Diagonal).
+
+diagonal_secundaria_aux([], _, _, []).
+diagonal_secundaria_aux([Row|Rows], Index, N, [Element|Diagonal]) :-
+    Pos is N - 1 - Index,
+    nth0(Pos, Row, Element),
+    NextIndex is Index + 1,
+    diagonal_secundaria_aux(Rows, NextIndex, N, Diagonal).
+
+lists_firsts_rests([], [], []).
+lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
+    lists_firsts_rests(Rest, Fs, Oss).
